8e48636c16cd0403c8a4c19eb5bbaf1bf2cd4583
Samius Sazin <samiussazin45@gmail.com>
2024-08-26 14:32:38 +0600

    Next js project

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedSection from "@/components/FeaturedSection";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import MeetOurInstructor from "@/components/MeetOurInstructor";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedSection />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <MeetOurInstructor />
+      <Footer />
+    </main>
+  );
+}
